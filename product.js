96911a57dcabe6ed8a6275d8a045a559e7133b75
krAbhishek09 <abhishekbhagat6654@gmail.com>
2024-06-03 23:41:38 +0530

    initial

---
+++ product.js
@@ -0,0 +1,8 @@
+let productData=[
+    {
+        "id":1,
+         "name":"samsung",
+         "price":24999,
+         "img":'https://m.media-amazon.com/images/I/71669TpsvKL._SL1500_.jpg'
+    }
+]
